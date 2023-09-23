04a51655f23e915c96ec5bb12824e7b977f7c89a
Aufaarib <aufa@nuncorp.id>
2023-09-23 10:24:38 +0700

    Update Show Date Picker On Input Focused

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -247,6 +247,7 @@ export const DropdownSiswa = ({
             options={options}
             onChange={onChange}
             isSearchable={isSearchable}
+            onKeyDown={onKeyDown}
           />
         </div>
       </form>
